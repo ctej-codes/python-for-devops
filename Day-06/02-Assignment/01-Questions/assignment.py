af4cff97661bb40f6ce546d2164eff2e4bdda163
Charan Tej <96952848+ctej-codes@users.noreply.github.com>
2026-08-27 10:16:09 +0000

    arthmetic operator assignment

---
+++ Day-06/02-Assignment/01-Questions/assignment.py
@@ -0,0 +1,52 @@
+##############################
+# Task 1: Arithmetic Operators
+################################
+a = 10
+b = 2
+
+# sum = a + b
+# print(sum)
+
+# diff = a - b
+# print(diff)
+
+# quot = a / b
+# print(quot)
+
+##############################
+# Task 2: Comparison Operators
+################################
+#Compare the values of a and b using the following comparison operators: <, >, <=, >=, ==, and !=.
+# print(a > b)
+# print (a < b)
+# print(a <= b) 
+# print(a >= b)
+# print(a == b)
+# print(a != b)
+
+##############################
+# Task 3: Logical Operators
+################################
+# x = True
+# y = False
+
+# and_check = x and y
+# print(and_check)
+
+# or_check = x or y
+# print(or_check)
+
+# not_check = not y
+# print(not_check)
+
+##############################
+# Task 4: Assignment Operators
+################################
+total = 10
+#Use assignment operators (+=, -=, *=, /=) to update the value of total
+# total+=total
+# total-=total
+# total*=total
+total/=total
+
+print(total)
