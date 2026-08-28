c338d2bc64014c44f9c2736e88dc970fcaaaa4b9
Charan Tej <96952848+ctej-codes@users.noreply.github.com>
2026-08-28 12:34:48 +0000

    file path script

---
+++ Day-10/folders_lst.py
@@ -0,0 +1,13 @@
+import os
+
+folders_lst = input("Enter the folder names seperated by a space: ").split()
+
+for folder in folders_lst:
+    try:
+        files = os.listdir(folder)
+        print ("======Printing the files in folder: ", folder)
+        for file in files:
+            print(file)
+    except FileNotFoundError:
+        print("Enter a valid folder name", "as", folder, "is not found")
+        continue
