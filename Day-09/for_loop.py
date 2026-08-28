cd928b9d51c9dd7bcfaea726c57ba04cddc40000
Charan Tej <96952848+ctej-codes@users.noreply.github.com>
2026-08-28 10:03:47 +0000

    loop program

---
+++ Day-09/for_loop.py
@@ -0,0 +1,13 @@
+# for i in range(1,10):
+#     print(i)
+
+log_file = [
+   "INFO: Operation successful",
+   "ERROR: File not found",
+   "DEBUG: Connection established",
+   "ERROR: Database connection failed",
+]
+
+for line in log_file:
+    if "ERROR" in line:
+        print(line)
